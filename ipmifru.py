02b32b4df4ab42e47e8c68dd6e4baa8e282893a6
YorikoSC <perov.sync@ya.ru>
2023-06-05 16:40:43 +0300

    Update ipmifru.py

--- ipmifru.py
+++ ipmifru.py
@@ -3,12 +3,11 @@ import subprocess
 from sys import executable
 
 dev = int(input('Gigabyte(1) или ASUS(2): '))#Выбор девелопера.
-while dev == 1: #Выбор режима обновления FRU.
-    if dev == 1:
+if dev == 1:
         loc = int(input('Локально(1) или удалённо(2): '))
-    elif dev == 2: #Асуса нет
+elif dev == 2: #Асуса нет
         print ('Данный функционал на данный момент не реализован')
-        dev == 1
+        quit()
 
     
 def multifru_net(): #Функция обновления полей(удаленно) при наличии двух FRU на сервере
